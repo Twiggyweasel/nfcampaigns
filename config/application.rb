8d467fccbab93635c8912541a0b7fd2091f4d663
twiggyweasel <orsusbass@gmail.com>
2017-01-17 23:03:55 +0000

    updated initializer for time_zone_aware_types

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Nfcampaigns
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
 end
 
 
-config.active_record.time_zone_aware_types = [:datetime, :time]
