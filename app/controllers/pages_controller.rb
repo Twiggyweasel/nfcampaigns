5b9ace3aaef51023484975c4c90af6bac8682c4a
twiggyweasel <orsusbass@gmail.com>
2017-02-04 23:55:16 +0000

    update layout file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  layout "index"
-  
   def home
     @top_attendees = Attendee.order(raised: :desc).limit(5)
     @top_teams = Team.where.not(name: 'No Team').order(raised: :desc).limit(5)
