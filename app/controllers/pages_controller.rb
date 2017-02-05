6f5a988ffe7adb59feb6d43532af57ca29c8a6d9
twiggyweasel <orsusbass@gmail.com>
2017-02-05 00:07:36 +0000

    style changes to home page and team index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  
   def home
     @top_attendees = Attendee.order(raised: :desc).limit(5)
     @top_teams = Team.where.not(name: 'No Team').order(raised: :desc).limit(5)
