7f23da01d6c3713f0ff394852bb4bfaf49620048
twiggyweasel <orsusbass@gmail.com>
2017-02-07 17:17:30 +0000

    New User email updates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'orsusbass@gmail.com'
+  default from: 'nfcampaigns@nfnetwork.org'
   
   def welcome_email(user)
     @user = user
-    @url = 'https://nf-event-manager-twiggyweasel.c9users.io/login'
+    @url = 'https://nfcampaigns.herokuapp.com/login'
     mail(to: @user.email, subject: 'Welcome to NFCampaigns')
   end
 end
