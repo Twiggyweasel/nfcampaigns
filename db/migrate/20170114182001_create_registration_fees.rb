5130479772454e3b6b6052f99e0608e0f02fce2c
twiggyweasel <orsusbass@gmail.com>
2017-02-20 05:24:44 +0000

    updates to models: payments, registration fees and events

--- db/migrate/20170114182001_create_registration_fees.rb
+++ db/migrate/20170114182001_create_registration_fees.rb
@@ -2,6 +2,7 @@ class CreateRegistrationFees < ActiveRecord::Migration[5.0]
   def change
     create_table :registration_fees do |t|
       t.string :name
+      t.string :registration_type
       t.decimal :amount
       
       t.references :event, foreign_key: true
