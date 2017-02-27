dff974b95bcee582ea60de3ffd099b21ee77cd92
twiggyweasel <orsusbass@gmail.com>
2017-02-27 01:16:01 +0000

    update applications form

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,8 @@
 class Application < ApplicationRecord 
   validates :name, presence: true, length: { minimum: 6 }
   validates :email, presence: true
-  validates :phone, presence: true, length: { is: 10 }
+  validates :phone, presence: true
+  validates :event_type, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
 end
