b9239a666c7f337f6eba1fe58c6208412df0795b
twiggyweasel <orsusbass@gmail.com>
2017-02-27 02:55:56 +0000

    attendee confirmation email change

--- app/mailers/attendee_confirmation_mailer.rb
+++ app/mailers/attendee_confirmation_mailer.rb
@@ -1,9 +1,9 @@
 class AttendeeConfirmationMailer < ApplicationMailer
+  default from: 'orsusbass@gmail.com'
   
   def confirmation(user, attendee_record)
     @user = user
     @attendee = attendee_record
-    @url = event_attendee_path(@attendee.event_id, @attendee.id)
     mail(to: @user.email, subject: '#NFStrong Registration Confirmation')
   end
   
