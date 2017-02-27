30ef8392b198adce3881d93c66f9916c1189b755
twiggyweasel <orsusbass@gmail.com>
2017-02-27 01:48:15 +0000

    Welcome email tweak

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'http://www.nfstrong.org/login'
-    mail(to: @user.email, subject: 'Welcome to NFCampaigns')
+    mail(to: @user.email, subject: 'Welcome to #NFStrong Events')
   end
 end
