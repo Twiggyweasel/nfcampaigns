c55eb5c285e27fb2820d1dc95130a8fc3c278c57
twiggyweasel <orsusbass@gmail.com>
2017-03-06 14:34:13 +0000

    change to event routing

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -27,7 +27,7 @@ class TeamsController < ApplicationController
   
   private 
     def set_event
-      @event = Event.find_by_title(params[:event_id])
+      @event = Event.find_by_title(params[:event])
     end
     
     def team_params
