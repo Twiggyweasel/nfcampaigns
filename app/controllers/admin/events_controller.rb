de30619c1e392f809b929ac1d5152a0f99c8034f
twiggyweasel <orsusbass@gmail.com>
2017-03-06 04:13:36 +0000

    event broken url

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -47,7 +47,7 @@ class Admin::EventsController < ApplicationController
   private
   
     def set_event 
-      @event = Event.find_by_title(params[:event_id])
+      @event = Event.find_by_title(params[:id])
     end
     def event_params
       params.require(:event).permit(:name, :event_cover, :event_card, :event_type, :desc, :teaser, :registration_date, :event_date, :event_start_time, :event_end_time, :goal, :raised, :venue_name, :street, :city, :state, :zipcode, :has_shirts, :is_private, size_ids: [])
