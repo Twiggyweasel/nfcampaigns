0dbbe28f079362649db0bd95a212f3617fc129b7
twiggyweasel <orsusbass@gmail.com>
2017-03-16 18:41:23 +0000

    testing admin update emails

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,8 +5,8 @@ class AdminMailer < ApplicationMailer
   #
   #   en.admin_mailer.new_users.subject
   #
-  def new_users(admin)
-    @admin = admin    
-    mail(to: admin.email, subject: "NFStrong - New User Summary")
+  def new_users(user)
+    @user = user    
+    mail(to: user.email, subject: "NFStrong - New User Summary")
   end
 end
--- lib/tasks/admin_updates.rake
+++ lib/tasks/admin_updates.rake
@@ -1,8 +1,8 @@
 desc "This task sends admins all new users"
 task :send_new_users => :environment do
   puts "building new user summary"
-  User.admins.each do |admin|
-    AdminMailer.new_users(admin).deliver_later
+  User.admins.each do |user|
+    AdminMailer.new_users(user).deliver_later
   end
   puts "done."
 end
