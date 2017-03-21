efe3d318e655683f8baf6d1173b50424b8ff1a50
twiggyweasel <orsusbass@gmail.com>
2017-03-21 02:59:50 +0000

    added teams management to events

---
+++ app/controllers/admin/teams_controller.rb
@@ -0,0 +1,49 @@
+class Admin::TeamsController < ApplicationController
+  layout 'admin'
+  before_action :set_event
+  def index 
+  end
+  
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = @event.teams.create(team_params)
+    
+    if @team.save
+      redirect_to admin_event_teams_path(@event), :flash => { :success => "Team successfully created" }
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @team = Team.find(params[:id])
+  end
+  
+  def update
+    @team = Team.find(params[:id])
+    
+    if @team.update(team_params)
+      redirect_to admin_event_teams_path(@event), :flash => { :success => "Team successfully updated" }
+    else
+      render :edit
+    end
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private 
+  
+  def set_event
+    @event = Event.find_by_title(params[:event_id])
+  end
+  
+  def team_params
+    params.require(:team).permit(:name, :goal, :max_members)
+  end
+end
