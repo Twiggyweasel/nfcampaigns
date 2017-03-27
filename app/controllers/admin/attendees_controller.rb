9c2a1efa60b899df9955598a499335ae874262b2
twiggyweasel <orsusbass@gmail.com>
2017-03-27 16:09:42 +0000

    updates to attendees add/edit

--- app/controllers/admin/attendees_controller.rb
+++ app/controllers/admin/attendees_controller.rb
@@ -7,6 +7,20 @@ class Admin::AttendeesController < ApplicationController
     @attendees = Attendee.includes(:pledge_page).where(event_id: @event.id)
   end
   
+  def new
+    @attendee = @event.attendees.new
+  end
+  
+  def create
+    @attendee = @event.attendees.create(attendee_params)
+    
+    if @attendee.save
+      redirect_to admin_event_attendees_path(@event), :flash => { :success => "Attendee Created!"}
+    else 
+      render :new
+    end
+  end
+  
   def edit 
     @attendee = Attendee.find(params[:id])
   end
@@ -27,6 +41,6 @@ class Admin::AttendeesController < ApplicationController
     end
     
     def attendee_params
-      params.require(:attendee).permit(:user_id, :team_id)
+      params.require(:attendee).permit(:user_id, :team_id, :fee, :shirt_size, :category, :paid)
     end
 end
