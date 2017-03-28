dd50ddd610cb3dba1d4627dddf08753bf33a73d6
twiggyweasel <orsusbass@gmail.com>
2017-03-28 01:47:13 +0000

    better handling of repeat email accounts

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -53,7 +53,7 @@ class SessionController < ApplicationController
         else
           # otherwise we have to create a user with the auth hash
           if !User.find_by_email(auth['info']['email']).blank?
-            render register_path, :flash => { :warning => "Your email address is already associated with an account, please sign in using or reset your password." }
+            redirect_to login_path, :flash => { :warning => "Your email address is already associated with an account, please sign in using or reset your password." }
           else
             u = User.create_with_omniauth(auth)
           end
