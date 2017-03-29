36b0453d524be441acb56afe267e2e1d321a9e94
twiggyweasel <orsusbass@gmail.com>
2017-03-29 03:42:35 +0000

    attendees update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321014603) do
+ActiveRecord::Schema.define(version: 20170329031720) do
 
   create_table "applications", force: :cascade do |t|
     t.string   "name"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20170321014603) do
     t.integer  "attendee_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "paid"
     t.index ["attendee_id"], name: "index_guests_on_attendee_id"
   end
 
