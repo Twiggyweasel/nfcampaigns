114a4a7bd68409e06319af6ee53883abdb7c9f48
twiggyweasel <orsusbass@gmail.com>
2017-03-31 04:39:27 +0000

    correcting migration for orders model

--- db/migrate/20170331010228_create_orders.rb
+++ db/migrate/20170331010228_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.boolean :paid, defaultL false
+      t.boolean :paid, default: false
       t.references :event, foreign_key: true
       t.references :user, foreign_key: true
     end
