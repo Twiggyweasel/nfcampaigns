7e6f38bc819039ac5051a44aadaa45529881fd23
twiggyweasel <orsusbass@gmail.com>
2017-05-03 14:31:05 +0000

    added better user info update support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-  
-  def index 
-    # @users = User.where.not(id: 1).order(:name).page params[:page] 
+
+  def index
+    # @users = User.where.not(id: 1).order(:name).page params[:page]
     @q = User.where.not(id: 1).order(:name).ransack(params[:q])
-    @users = @q.result().page params[:page] 
+    @users = @q.result().page params[:page]
   end
-  
-  def show 
+
+  def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = env['omniauth.identity'] ||= User.new
   end
-  
-  # def create 
+
+  # def create
 
   #   @user = User.new(user_params)
   #   if @user.save
@@ -28,13 +28,25 @@ class UsersController < ApplicationController
 
   # end
 
-  def edit 
+  def edit
     @user = User.find(params[:id])
+    @profile = @user.profile
   end
-  
-  private 
-  
+
+  def update
+    @user = User.find(params[:id])
+    @profile = @user.profile
+
+    if @user.update(user_params)
+      redirect_to user_account_settings_path(@user), :flash => { :success => "profile successfully updated"}
+    else
+      render edit
+    end
+  end
+
+  private
+
     def user_params
-      params.required(:user).permit(:email, :password, :role_id)
+      params.required(:user).permit(:name, :email, :password, :role_id, :profile_pic, profile_attributes: [:street, :apt, :city, :state, :zipcode] )
     end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
-  belongs_to :user 
-  
+  belongs_to :user
+
   validates_uniqueness_of :user_id, :message => "You can only create one profile"
   validates :city, presence: true
   validates :state, presence: true
-  
+
 end
