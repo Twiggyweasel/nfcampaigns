18af1c7db53aba533c8192e92796abf3ee50318a
twiggyweasel <orsusbass@gmail.com>
2017-05-24 02:06:39 +0000

    added search to admin users list

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,38 +2,39 @@ class Admin::UsersController < ApplicationController
   layout "admin"
   before_action :set_user, except: [:index, :new, :create]
   before_action :require_user, :require_admin
-  
+
   def index
-    @users = User.where.not(id: current_user.id).order(created_at: :desc)
+    @q = User.where.not(id: current_user.id).order(:name).ransack(params[:q])
+    @users = @q.result().order(created_at: :desc).page params[:page]
     respond_to do |format|
-      format.html 
+      format.html
       format.xlsx
     end
   end
-  
+
   def show
-    
+
   end
-  
-  def new 
+
+  def new
     @user = User.new
     @user.build_profile
   end
-  
-  def create 
+
+  def create
     @user = User.new(user_params)
-    
+
     if @user.save
       redirect_to admin_user_path(@user), :flash => { :success => "User Successfully Created!" }
     else
       render :new
     end
   end
-  
-  def edit 
-    
+
+  def edit
+
   end
-  
+
   def update
     if @user.update(user_params)
       redirect_to admin_users_path, :flash => { :success => "User record successfully updated"}
@@ -41,17 +42,17 @@ class Admin::UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
-    @user.destroy 
+    @user.destroy
     redirect_to admin_user_path, :flash => { :sucess => "User Successfully Deleted" }
   end
-  
+
   private
     def set_user
       @user = User.find(params[:id])
     end
-    
+
     def user_params
       params.require(:user).permit(:email, :name, :password, :role_id, profile_attributes: [:id, :street, :apt, :city, :state, :zipcode, :phone])
     end
