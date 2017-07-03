4c7a86582011cbc6833c9081817bb615ae9316d9
twiggyweasel <orsusbass@gmail.com>
2017-07-03 18:28:30 +0000

    linked application reference to show page, and updated title

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ApplicationsController < ApplicationController
   layout "admin"
   before_action :require_user, :require_admin
+
   def index
     @apps = Application.all
+  end
+
+  def show
+    @app = Application.find(params[:id])
   end
 end
